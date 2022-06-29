2f37d55cc4484dc8c61c36d03e815a4ac188769d
Do Trong Duc <ducdotrong2000@gmail.com>
2022-06-30 01:22:28 +0700

    copy assets

---
+++ src/repositories/tag.ts
@@ -0,0 +1,6 @@
+import { CreateTagInput, UpdateTagInput } from '@ts-types/generated';
+import Base from './base';
+
+class Tag extends Base<CreateTagInput, UpdateTagInput> {}
+
+export default new Tag();
